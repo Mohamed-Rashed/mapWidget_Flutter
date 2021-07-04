a64772b2bcdf948df83ef3af967360c3627bb7e1
Mohamed-Rashed <mohamedrashed88@gmail.com>
2021-07-04 14:15:34 +0200

    first

---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MapWidget(),
+    );
+  }
+}
+
+class MapWidget extends StatefulWidget {
+  const MapWidget({Key key}) : super(key: key);
+
+  @override
+  _MapWidgetState createState() => _MapWidgetState();
+}
+
+class _MapWidgetState extends State<MapWidget> {
+  LatLng currentPostion;
+  Map<MarkerId, Marker> markers = {};
+  Completer<GoogleMapController> _controller = Completer();
+  PolylinePoints polylinePoints = PolylinePoints();
+  Map<PolylineId, Polyline> polylines = {};
+
+  void _getUserLocation() async {
+    var position = await GeolocatorPlatform.instance
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      currentPostion = LatLng(position.latitude, position.longitude);
+      _addMarker(
+        LatLng(currentPostion.latitude, currentPostion.longitude),
+        "origin",
+        BitmapDescriptor.defaultMarker,
+        true
+      );
+
+      // Add destination marker
+      _addMarker(
+        LatLng(31.2252, 29.9419),
+        "destination",
+        BitmapDescriptor.defaultMarkerWithHue(240),
+        false
+      );
+      _getPolyline();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserLocation();
+  }
+
+  _addMarker(LatLng position, String id, BitmapDescriptor descriptor,bool draggable) {
+    MarkerId markerId = MarkerId(id);
+    Marker marker = Marker(
+        markerId: markerId,
+        icon: descriptor,
+        position: position,
+        draggable: true,
+        onDragEnd: ((newPosition) {
+          setState(() {
+            currentPostion = newPosition;
+          });
+        }));
+    markers[markerId] = marker;
+  }
+
+  void _getPolyline() async {
+    List<LatLng> polylineCoordinates = [];
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      "AIzaSyD2pHID7BXWg38sq6tqHKOHVvGQpLw-qWY",
+      PointLatLng(currentPostion.latitude, currentPostion.longitude),
+      PointLatLng(31.2252, 29.9419),
+      travelMode: TravelMode.driving,
+    );
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      });
+    } else {
+      print(result.errorMessage);
+    }
+    _addPolyLine(polylineCoordinates);
+  }
+
+  _addPolyLine(List<LatLng> polylineCoordinates) {
+    PolylineId id = PolylineId("poly");
+    Polyline polyline = Polyline(
+      color: Colors.red,
+      polylineId: id,
+      points: polylineCoordinates,
+      width: 8,
+    );
+    polylines[id] = polyline;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _getUserLocation();
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(currentPostion.latitude, currentPostion.longitude),
+          zoom: 17.4746,
+        ),
+        markers: Set<Marker>.of(markers.values),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        polylines: Set<Polyline>.of(polylines.values),
+      ),
+    );
+  }
+}
